ef9a892eb9aa069b2289ab4f370c4361bb07d4f4
sergey <kolmatsuy777555@gmail.com>
2024-08-29 10:55:57 +0300

    Added example

--- home work 4.3.py
+++ home work 4.3.py
@@ -1,7 +1,7 @@
 import random
 
-list  = random.randint(8, 10)  # Кількість елементів у списку від 3 до 10
-random_list = [random.randint(1, 10) for _ in range(list)]
+list  = random.randint(3, 10)  # Кількість елементів у списку від 3 до 10
+random_list = [random.randint(1, 10) for i in range(list)]
 
 first = 0
 third = 2
@@ -9,15 +9,16 @@ second_from_end = -2
 
 new_list = [random_list[first], random_list[third], random_list[second_from_end]]
 
-print("Вихідний список:", random_list)
-print("Новий список:", new_list)
+print(random_list)
+print(new_list)
+
 
 #####
 
 import random
 
 list  = random.randint(3, 5)  # Кількість елементів у списку від 3 до 10
-random_list = [random.randint(1, 10) for _ in range(list)]
+random_list = [random.randint(1, 10) for i in range(list)]
 
 first = 0
 third = 2
@@ -25,5 +26,5 @@ second_from_end = -2
 
 new_list = [random_list[first], random_list[third], random_list[second_from_end]]
 
-print("Вихідний список:", random_list)
-print("Новий список:", new_list)
+print(random_list)
+print(new_list)

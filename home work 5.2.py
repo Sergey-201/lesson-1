6132695a5db6932783d024e2a559a51a3a0a4da7
sergey <kolmatsuy777555@gmail.com>
2024-08-30 20:58:27 +0300

    Commit example

---
+++ home work 5.2.py
@@ -0,0 +1,29 @@
+while True:
+
+        n1 = float(input("Введите первое число: "))
+
+        operation = input("Введите действие (+, -, *, /): ")
+
+        n2 = float(input("Введите второе число: "))
+
+        match operation:
+            case '+':
+                result = n1 + n2
+            case '-':
+                result = n1 - n2
+            case '*':
+                result = n1 * n2
+            case '/':
+                if n2 == 0:
+                    print("Деление на ноль не возможно!")
+                else:
+                    result = n1 / n2
+
+        if operation in ['+', '-', '*', '/'] and not (operation == '/' and n2 == 0):
+
+            print(f"Результат: {result}")
+        continue_ = input("( для продолжения введите: y, для завершения введите: q  ): ").lower()
+
+        if continue_ == 'q':
+            print("Работа завершена.")
+            break

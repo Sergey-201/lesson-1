e9ef5b64727ade1283f18cd268e174b43f3b6769
sergey <kolmatsuy777555@gmail.com>
2024-09-02 23:45:23 +0300

    Commit example

---
+++ home work 5.3.py
@@ -0,0 +1,16 @@
+text = 'Python Community'
+
+delete = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+cleaned_text = ''.join(i for i in text if i not in delete)
+
+new_text = cleaned_text.split()
+
+capitalized_new_text = [word.capitalize() for word in new_text]
+
+hashtag = '#' + ''.join(capitalized_new_text)
+
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+
+print(hashtag)

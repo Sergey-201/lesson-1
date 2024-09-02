e9d75f6d6e1e4158899c3a856ac4ec48b1610873
sergey <kolmatsuy777555@gmail.com>
2024-09-02 23:53:43 +0300

    Commit example

--- home work 5.3.py
+++ home work 5.3.py
@@ -14,3 +14,20 @@ if len(hashtag) > 140:
     hashtag = hashtag[:140]
 
 print(hashtag)
+#####
+text = 't!e@s#t t%e^s&t'
+
+delete = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+cleaned_text = ''.join(i for i in text if i not in delete)
+
+new_text = cleaned_text.split()
+
+capitalized_new_text = [word.capitalize() for word in new_text]
+
+hashtag = '#' + ''.join(capitalized_new_text)
+
+if len(hashtag) > 140:
+    hashtag = hashtag[:140]
+
+print(hashtag)

c5ea22fdcca0c4247d5d4211b8fa47a6e4be2bb6
sergey <kolmatsuy777555@gmail.com>
2024-09-10 13:51:34 +0300

    Commit example

--- home work 7.1.py
+++ home work 7.1.py
@@ -3,7 +3,6 @@ def say_hi(name, age):
     print(result)
     return result
 
-# Перевірка функції
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 print('ОК')
